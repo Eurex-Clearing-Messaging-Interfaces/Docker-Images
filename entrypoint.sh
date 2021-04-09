8ae8c47fe4e882b99272214a9b161705d15add83
ai782 <antonin.vyborny@deutsche-boerse.com>
2021-04-09 12:01:39 +0200

    Fixml sim

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo "INFO:  Starting broker..."
-export QPID_PID_FILENAME="broker.pid" && export QPID_WORK="/home/oper/qpid/0000/broker/work" && export QPID_JAVA_MEM="-Xmx300m -XX:MaxDirectMemorySize=200m" && export JAVA_OPTS="-Dderby.stream.error.file=/dev/null -XX:+UnlockDiagnosticVMOptions -XX:+LogVMOutput -XX:LogFile=/home/oper/qpid/0000/log/ecag-fixml-dev1-amqp_broker_jvm-0000.log" && /home/oper/qpid/0000/broker/bin/qpid-server 2>&1 &
-
+export QPID_PID_FILENAME="broker.pid" && export QPID_WORK="/home/oper/qpid/0000/broker/work" && export QPID_JAVA_MEM="-Xmx300m -XX:MaxDirectMemorySize=200m" && export JAVA_OPTS="-Dderby.stream.error.file=/dev/null -XX:+UnlockDiagnosticVMOptions -XX:+LogVMOutput -XX:LogFile=/home/oper/qpid/0000/log/ecag-fixml-sim1-amqp_broker_jvm-0000.log" && /home/oper/qpid/0000/broker/bin/qpid-server 2>&1 &
 
 tail -f /dev/null
